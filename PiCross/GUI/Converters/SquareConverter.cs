debe2d378fcffe722787e575b31b04548d8b9d39
Frederic Vogels <frederic.vogels@gmail.com>
2015-06-14 11:14:12 +0200

    Beginnings of view models

---
+++ PiCross/GUI/Converters/SquareConverter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using PiCross.Game;
+
+namespace GUI.Converters
+{
+    public class SquareConverter : IValueConverter
+    {
+        public object Unknown { get; set; }
+
+        public object Empty { get; set; }
+
+        public object Filled { get; set; }
+
+        public object Convert( object value, Type targetType, object parameter, System.Globalization.CultureInfo culture )
+        {
+            var square = (Square) value;
+
+            if ( square == Square.UNKNOWN )
+            {
+                return Unknown;
+            }
+            else if ( square == Square.EMPTY )
+            {
+                return Empty;
+            }
+            else if ( square == Square.FILLED )
+            {
+                return Filled;
+            }
+            else
+            {
+                throw new ArgumentNullException( "null not supported" );
+            }
+        }
+
+        public object ConvertBack( object value, Type targetType, object parameter, System.Globalization.CultureInfo culture )
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- PiCross/GUI/MainWindow.xaml.cs
+++ PiCross/GUI/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using PiCross.DataStructures;
+using PiCross.Facade.Solving;
+using PiCross.Game;
 
 namespace GUI
 {
@@ -23,6 +26,119 @@ namespace GUI
         public MainWindow()
         {
             InitializeComponent();
+
+            var editorGrid = EditorGrid.FromStrings(
+                "..x..",
+                "..x..",
+                "xxxxx",
+                "..x..",
+                "..x.."
+                );
+
+            var playGrid = editorGrid.CreatePlayGrid();
+            playGrid.Squares.Overwrite( editorGrid.Squares );
+
+            this.DataContext = new GridViewModel( new Puzzle( playGrid ) );
+        }
+    }
+
+    public class PuzzleViewModel
+    {
+        private readonly IPuzzle puzzle;
+
+        private readonly GridViewModel grid;
+
+        private readonly ISequence<ConstraintsViewModel> columnConstraints;
+
+        private readonly ISequence<ConstraintsViewModel> rowConstraints;
+
+        public PuzzleViewModel(IPuzzle puzzle)
+        {
+            this.grid = new GridViewModel( puzzle );
+            // this.columnConstraints = puzzle.
+        }
+    }
+
+    public class GridViewModel
+    {
+        private readonly IPuzzle puzzle;
+
+        public GridViewModel( IPuzzle puzzle )
+        {
+            this.puzzle = puzzle;
+        }
+
+        public IEnumerable<IEnumerable<GridSquareViewModel>> Rows
+        {
+            get
+            {
+                return from y in Enumerable.Range( 0, puzzle.Height )
+                       select ( from x in Enumerable.Range( 0, puzzle.Width )
+                                let position = new Vector2D( x, y )
+                                select new GridSquareViewModel( puzzle[position] ) );
+            }
+        }
+    }
+
+    public class GridSquareViewModel
+    {
+        private readonly IPuzzleSquare square;
+
+        public GridSquareViewModel(IPuzzleSquare square)
+        {
+            this.square = square;
+        }
+
+        public ICell<Square> Contents
+        {
+            get
+            {
+                return square.Contents;
+            }
+        }
+    }
+
+    public class ConstraintsViewModel
+    {
+        private readonly IPuzzleConstraints constraints;
+
+        public ConstraintsViewModel(IPuzzleConstraints constraints)
+        {
+            this.constraints = constraints;
+        }
+
+        public IEnumerable<ConstraintsValueViewModel> Values
+        {
+            get
+            {
+                return constraints.Values.Items.Select( value => new ConstraintsValueViewModel( value ) );
+            }
+        }
+    }
+
+    public class ConstraintsValueViewModel
+    {
+        private readonly IPuzzleConstraintsValue value;
+
+        public ConstraintsValueViewModel(IPuzzleConstraintsValue value)
+        {
+            this.value = value;
+        }
+
+        public ICell<bool> IsSatisfied
+        {
+            get
+            {
+                return this.value.IsSatisfied;
+            }
+        }
+
+        public ICell<int> Number
+        {
+            get
+            {
+                return Cell.Create( this.value.Value );
+            }
         }
     }
 }
