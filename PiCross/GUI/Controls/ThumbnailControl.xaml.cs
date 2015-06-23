aa4ae79245591548a48380f5f56b7e8f29b4eb7b
Frederic Vogels <frederic.vogels@gmail.com>
2015-06-23 15:40:19 +0200

    Added ThumbnailControl

---
+++ PiCross/GUI/Controls/ThumbnailControl.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using PiCross.Cells;
+using PiCross.DataStructures;
+
+namespace GUI.Controls
+{
+    /// <summary>
+    /// Interaction logic for ThumbnailControl.xaml
+    /// </summary>
+    public partial class ThumbnailControl : UserControl
+    {
+        public ThumbnailControl()
+        {
+            InitializeComponent();
+        }
+
+        #region Grid
+
+        public IGrid<Cell<bool>> Grid
+        {
+            get { return (IGrid<Cell<bool>>) GetValue( GridProperty ); }
+            set { SetValue( GridProperty, value ); }
+        }
+
+        public static readonly DependencyProperty GridProperty =
+            DependencyProperty.Register( "Grid", typeof( IGrid<Cell<bool>> ), typeof( ThumbnailControl ), new PropertyMetadata( null ) );
+
+        #endregion
+
+        #region PixelSize
+
+        public double PixelSize
+        {
+            get { return (double) GetValue( PixelSizeProperty ); }
+            set { SetValue( PixelSizeProperty, value ); }
+        }
+
+        public static readonly DependencyProperty PixelSizeProperty =
+            DependencyProperty.Register( "PixelSize", typeof( double ), typeof( ThumbnailControl ), new PropertyMetadata( 2 ) );
+
+        #endregion
+    }
+}
