cb0099fa6147b71cf140065c3e7ec590524bd395
Frederic Vogels <frederic.vogels@gmail.com>
2015-07-01 17:11:34 +0200

    Moved ItemViewModels into separate file

---
+++ PiCross/GUI/ViewModels/PlayerSelection/ItemViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PiCross.Facade.IO;
+
+namespace GUI.ViewModels.PlayerSelection
+{
+    public abstract class ItemViewModel
+    {
+        private readonly PlayerSelectionViewModel parent;
+
+        protected ItemViewModel( PlayerSelectionViewModel parent )
+        {
+            this.parent = parent;
+        }
+
+        protected PlayerSelectionViewModel Parent
+        {
+            get
+            {
+                return parent;
+            }
+        }
+    }
+
+    public class SelectPlayerViewModel : ItemViewModel
+    {
+        private readonly IPlayerProfile userProfile;
+
+        public SelectPlayerViewModel( PlayerSelectionViewModel parent, IPlayerProfile userProfile )
+            : base( parent )
+        {
+            this.userProfile = userProfile;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return userProfile.Name;
+            }
+        }
+    }
+
+    public class AddPlayerViewModel : ItemViewModel
+    {
+        public AddPlayerViewModel( PlayerSelectionViewModel parent )
+            : base( parent )
+        {
+            // NOP
+        }
+    }
+}
--- PiCross/GUI/ViewModels/PlayerSelection/PlayerSelectionViewModel.cs
+++ PiCross/GUI/ViewModels/PlayerSelection/PlayerSelectionViewModel.cs
@@ -40,49 +40,5 @@ namespace GUI.ViewModels.PlayerSelection
         }
     }
 
-    public abstract class ItemViewModel
-    {
-        private readonly PlayerSelectionViewModel parent;
-
-        protected ItemViewModel( PlayerSelectionViewModel parent )
-        {
-            this.parent = parent;
-        }
-
-        protected PlayerSelectionViewModel Parent
-        {
-            get
-            {
-                return parent;
-            }
-        }
-    }
-
-    public class SelectPlayerViewModel : ItemViewModel
-    {
-        private readonly IPlayerProfile userProfile;
-
-        public SelectPlayerViewModel( PlayerSelectionViewModel parent, IPlayerProfile userProfile )
-            : base( parent )
-        {
-            this.userProfile = userProfile;
-        }
-
-        public string Name
-        {
-            get
-            {
-                return userProfile.Name;
-            }
-        }
-    }
-
-    public class AddPlayerViewModel : ItemViewModel
-    {
-        public AddPlayerViewModel( PlayerSelectionViewModel parent )
-            : base( parent )
-        {
-            // NOP
-        }
-    }
+    
 }
