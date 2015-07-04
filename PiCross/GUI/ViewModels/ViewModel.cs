837004a18778b735b07c6b0a92bf0d082e4cac66
Frederic Vogels <frederic.vogels@gmail.com>
2015-07-04 12:59:51 +0200

    Added activation and deactivation for viewmodels

--- PiCross/GUI/ViewModels/ViewModel.cs
+++ PiCross/GUI/ViewModels/ViewModel.cs
@@ -25,12 +25,26 @@ namespace GUI.ViewModels
 
         protected void Push(ViewModel viewModel)
         {
+            Parent.ActiveViewModel.Value.OnDeactivation();
             Parent.PushViewModel( viewModel );
+            Parent.ActiveViewModel.Value.OnActivation();
         }
 
         protected void Pop()
         {
+            Parent.ActiveViewModel.Value.OnDeactivation();
             Parent.PopViewModel();
+            Parent.ActiveViewModel.Value.OnActivation();
+        }
+
+        protected virtual void OnActivation()
+        {
+            // NOP
+        }
+
+        protected virtual void OnDeactivation()
+        {
+            // NOP
         }
     }
 }
