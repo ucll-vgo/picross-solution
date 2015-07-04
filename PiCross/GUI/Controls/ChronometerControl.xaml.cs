6575e6964385ec61cacdb8389f3b3fd48e352324
Frederic Vogels <frederic.vogels@gmail.com>
2015-07-04 20:02:34 +0200

    Added ChronometerControl

---
+++ PiCross/GUI/Controls/ChronometerControl.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GUI.Controls
+{
+    /// <summary>
+    /// Interaction logic for ChronometerControl.xaml
+    /// </summary>
+    public partial class ChronometerControl : UserControl
+    {
+        public ChronometerControl()
+        {
+            InitializeComponent();
+        }
+
+        public DateTime ElapsedTime
+        {
+            get { return (DateTime) GetValue( ElapsedTimeProperty ); }
+            set { SetValue( ElapsedTimeProperty, value ); }
+        }
+
+        public static readonly DependencyProperty ElapsedTimeProperty =
+            DependencyProperty.Register( "ElapsedTime", typeof( DateTime ), typeof( ChronometerControl ), new PropertyMetadata( TimeSpan.Zero, ( obj, args ) => ( (ChronometerControl) obj ).OnTimeElapsedChanged( args ) ) );
+
+        private void OnTimeElapsedChanged( DependencyPropertyChangedEventArgs args )
+        {
+            UpdateHands();
+        }
+
+        private void UpdateHands()
+        {
+            UpdateSecondHand();
+            UpdateMinuteHand();
+        }
+
+        private void UpdateSecondHand()
+        {
+            secondsTransform.Angle = TransformToAngle( ElapsedTime.Second );
+        }
+
+        private void UpdateMinuteHand()
+        {
+            minutesTransform.Angle = TransformToAngle( ElapsedTime.Minute );
+        }
+
+        private double TransformToAngle( double handPosition )
+        {
+            return handPosition * 60;
+        }
+    }
+}
