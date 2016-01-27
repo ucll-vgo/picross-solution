b93e1af41c3268c648332b735c4c065c9decf8dc
Frederic Vogels <frederic.vogels@gmail.com>
2016-01-27 20:00:28 +0100

    Added animated logo

--- PiCross/GUI/ViewModels/Intro/IntroViewModel.cs
+++ PiCross/GUI/ViewModels/Intro/IntroViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,7 +8,10 @@ using System.Windows.Input;
 using GUI.Commands;
 using GUI.ViewModels.LibraryMode;
 using GUI.ViewModels.PlayerSelection;
+using PiCross.Cells;
+using PiCross.DataStructures;
 using PiCross.Facade.IO;
+using PiCross.Game;
 
 namespace GUI.ViewModels.Intro
 {
@@ -19,12 +23,79 @@ namespace GUI.ViewModels.Intro
 
         private readonly ICommand quit;
 
+        private readonly StepwiseSolver solver;
+
+        private readonly IGrid<Cell<bool>> logoGrid;
+
+        private Scheduler scheduler;
+
         public IntroViewModel( MasterController parent )
             : base( parent )
         {
             play = EnabledCommand.FromDelegate( PerformPlay );
             edit = EnabledCommand.FromDelegate( PerformEdit );
             quit = EnabledCommand.FromDelegate( PerformQuit );
+
+            var puzzle = CreatePiCrossLogo();
+            var solverGrid = SolverGrid.FromPuzzle( puzzle );
+            solver = new StepwiseSolver( solverGrid );
+            logoGrid = solverGrid.Squares.Map( ( Square square ) => Cell.Create( false ) ).Copy();
+
+            ScheduleUpdate();
+        }
+
+        private void SynchronizeLogoGridWithSolverGrid()
+        {
+            foreach ( var position in logoGrid.AllPositions )
+            {
+                logoGrid[position].Value = solver.Grid.Squares[position] == Square.FILLED;
+            }
+        }
+
+        private void UpdateLogoGrid()
+        {
+            solver.Step();
+            SynchronizeLogoGridWithSolverGrid();
+
+            ScheduleUpdate();
+        }
+
+        private void ScheduleUpdate()
+        {
+            scheduler = new Scheduler( 1, UpdateLogoGrid );
+        }
+
+        private Puzzle CreatePiCrossLogo()
+        {
+            //var strings = new[] { ".................................",
+            //                      "xxxx.....xxx.....................",
+            //                      "x...x...x........................",
+            //                      "x...x.x.x........................",
+            //                      "xxxx....x....x.xx..xx...xxx..xxx.",
+            //                      "x.....x.x....xx...x..x.x....x....",
+            //                      "x.....x.x....x....x..x..xx...xx..",
+            //                      "x.....x.x....x....x..x....x....x.",
+            //                      "x.....x..xxx.x.....xx..xxx..xxx..",
+            //                      "................................." };
+
+            var strings = new[] { ".xx.xx.",
+                                  "...x...",
+                                  "...x...",
+                                  ".xx.xx." };
+
+            var puzzle = Puzzle.FromRowStrings( strings );
+
+            Debug.Assert( puzzle.IsSolvable );
+
+            return puzzle;
+        }
+
+        public IGrid<Cell<bool>> LogoGrid
+        {
+            get
+            {
+                return logoGrid;
+            }
         }
 
         public ICommand Play
@@ -64,6 +135,11 @@ namespace GUI.ViewModels.Intro
         private void PerformQuit()
         {
             Pop();
+        }
+
+        public override void OnTick( double dt )
+        {
+            scheduler.Tick( dt );
         }
     }
 }
