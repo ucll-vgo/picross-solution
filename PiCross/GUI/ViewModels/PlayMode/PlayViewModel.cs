1a9c9cc030dafc678c85d4f8b52c7781b637a661
Frederic Vogels <frederic.vogels@gmail.com>
2016-01-30 15:15:58 +0100

    Refactored PlayViewModel

--- PiCross/GUI/ViewModels/PlayMode/PlayViewModel.cs
+++ PiCross/GUI/ViewModels/PlayMode/PlayViewModel.cs
@@ -492,5 +492,45 @@ namespace GUI.ViewModels.PlayMode
                 }
             }
         }
+
+        public class ConstraintsViewModel
+        {
+            private readonly IPlayablePuzzleConstraints constraints;
+
+            private readonly ISequence<ConstraintsValueViewModel> values;
+
+            private readonly Cell<bool> active;
+
+            public ConstraintsViewModel( IPlayablePuzzleConstraints constraints, Cell<bool> active )
+            {
+                this.constraints = constraints;
+                this.active = active;
+                this.values = constraints.Values.Map( val => new ConstraintsValueViewModel( val ) ).Copy();
+            }
+
+            public Cell<bool> IsSatisfied
+            {
+                get
+                {
+                    return constraints.IsSatisfied;
+                }
+            }
+
+            public ISequence<ConstraintsValueViewModel> Values
+            {
+                get
+                {
+                    return values;
+                }
+            }
+
+            public Cell<bool> IsActive
+            {
+                get
+                {
+                    return active;
+                }
+            }
+        }
     }
 }
