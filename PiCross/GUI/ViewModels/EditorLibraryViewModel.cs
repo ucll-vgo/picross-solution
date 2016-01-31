2a2d613d12b249af7eb88ccaf56a80d36a9cf17b
Frederic Vogels <frederic.vogels@gmail.com>
2016-01-31 18:32:37 +0100

    Synced with domain

--- PiCross/GUI/ViewModels/EditorLibraryViewModel.cs
+++ PiCross/GUI/ViewModels/EditorLibraryViewModel.cs
@@ -10,6 +10,7 @@ using DataStructures;
 using PiCross.Facade.Editing;
 using PiCross.Facade.IO;
 using PiCross.Game;
+using System.Text.RegularExpressions;
 
 namespace GUI.ViewModels
 {
@@ -21,17 +22,27 @@ namespace GUI.ViewModels
 
         private readonly ICommand select;
 
+        private readonly ICommand create;
+
         private readonly Cell<IEnumerable<GroupViewModel>> groups;
 
-        public EditorLibraryViewModel( MasterController parent, ILibrary library ) 
-            : base(parent)
+        public EditorLibraryViewModel( MasterController parent, ILibrary library )
+            : base( parent )
         {
             this.library = library;
             this.back = EnabledCommand.FromDelegate( PerformBack );
             this.select = EnabledCommand.FromDelegate<EntryViewModel>( PerformSelect );
-            this.groups = Cell.Create( from entry in library.Entries
-                                       group entry by entry.Puzzle.Size into entryGroup
-                                       select new GroupViewModel( entryGroup.Key, entryGroup ) );
+            this.create = EnabledCommand.FromDelegate<string>( PerformCreate );
+            this.groups = Cell.Create<IEnumerable<GroupViewModel>>( null );
+
+            UpdateGroups();
+        }
+
+        private void UpdateGroups()
+        {
+            this.groups.Value = from entry in library.Entries
+                                group entry by entry.Puzzle.Size into entryGroup
+                                select new GroupViewModel( entryGroup.Key, entryGroup );
         }
 
         public ICommand Back
@@ -50,14 +61,42 @@ namespace GUI.ViewModels
             }
         }
 
+        public ICommand Create
+        {
+            get
+            {
+                return create;
+            }
+        }
+
         private void PerformBack()
         {
             Pop();
         }
 
-        private void PerformSelect(EntryViewModel entry)
+        private void PerformSelect( EntryViewModel entry )
         {
             Push( new EditorViewModel( Parent, entry.LibraryEntry ) );
+        }
+
+        private void PerformCreate( string sizeString )
+        {
+            var match = Regex.Match( sizeString, @"^(\d+)x(\d+)$" );
+
+            if ( !match.Success )
+            {
+                throw new ArgumentException( "BUG" );
+            }
+            else
+            {
+                var width = int.Parse( match.Groups[1].Value );
+                var height = int.Parse( match.Groups[2].Value );
+                var size = new Size( width, height );
+
+                var newEntry = library.Create( size, "unnamed" );
+
+                UpdateGroups();
+            }
         }
 
         public Cell<IEnumerable<GroupViewModel>> Groups
@@ -67,14 +106,14 @@ namespace GUI.ViewModels
                 return groups;
             }
         }
-        
+
         public class GroupViewModel
         {
             private readonly Size size;
 
             private readonly IEnumerable<ILibraryEntry> entries;
 
-            public GroupViewModel(Size size, IEnumerable<ILibraryEntry> entries)
+            public GroupViewModel( Size size, IEnumerable<ILibraryEntry> entries )
             {
                 this.size = size;
                 this.entries = entries;
@@ -102,7 +141,7 @@ namespace GUI.ViewModels
         {
             private readonly ILibraryEntry entry;
 
-            public EntryViewModel(ILibraryEntry entry)
+            public EntryViewModel( ILibraryEntry entry )
             {
                 this.entry = entry;
             }
@@ -111,7 +150,7 @@ namespace GUI.ViewModels
             {
                 get
                 {
-                    return entry.Puzzle.Grid.Map((bool x) => Cell.Create(x)).Copy();
+                    return entry.Puzzle.Grid.Map( ( bool x ) => Cell.Create( x ) ).Copy();
                 }
             }
 
