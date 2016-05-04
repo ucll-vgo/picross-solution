a8ffdfbfdabd98e0b748fac33d60a86264883437
Frederic Vogels <frederic.vogels@gmail.com>
2016-05-04 20:02:50 +0200

    PiCon

--- PiCross/PiCon/Program.cs
+++ PiCross/PiCon/Program.cs
@@ -5,50 +5,54 @@ using System.Text;
 using System.Threading.Tasks;
 using DataStructures;
 using PiCross;
+using System.IO;
 
 namespace PiCon
 {
     class Program
     {
-        static void Main( string[] args )
+        static void Main(string[] args)
         {
+            // var reader = new StreamReader(@"E:\repos\ucll\vgo\picross\private\Data\bla.txt");
+            var reader = Console.In;
+            Solve(reader);
         }
 
-        private static void Solve()
+        private static void Solve(TextReader reader)
         {
-            var dimensions = ReadInts();
+            var dimensions = ReadInts(reader);
             var width = dimensions[0];
             var height = dimensions[1];
 
             var columnConstraints = new List<Constraints>();
             var rowConstraints = new List<Constraints>();
 
-            for ( var i = 0; i != width; ++i )
+            for (var i = 0; i != width; ++i)
             {
-                var ns = ReadInts();
-                columnConstraints.Add( Constraints.FromValues( ns ) );
+                var ns = ReadInts(reader);
+                columnConstraints.Add(Constraints.FromValues(ns));
             }
 
-            for ( var i = 0; i != height; ++i )
+            for (var i = 0; i != height; ++i)
             {
-                var ns = ReadInts();
-                rowConstraints.Add( Constraints.FromValues( ns ) );
+                var ns = ReadInts(reader);
+                rowConstraints.Add(Constraints.FromValues(ns));
             }
 
-            var puzzle = Puzzle.FromConstraints( columnConstraints: Sequence.FromEnumerable( columnConstraints ), rowConstraints: Sequence.FromEnumerable( rowConstraints ) );
+            var puzzle = Puzzle.FromConstraints(columnConstraints: Sequence.FromEnumerable(columnConstraints), rowConstraints: Sequence.FromEnumerable(rowConstraints));
 
-            Console.WriteLine( "Woumpousse" );
-            Console.WriteLine( "{0} {1}", width, height );
+            Console.WriteLine("Woumpousse");
+            Console.WriteLine("{0} {1}", width, height);
 
-            foreach ( var row in puzzle.Grid.Rows )
+            foreach (var row in puzzle.Grid.Rows)
             {
-                Console.WriteLine( row.Map( b => b ? 'x' : '.' ).AsString() );
+                Console.WriteLine(row.Map(b => b ? 'x' : '.').AsString());
             }
         }
 
-        private static int[] ReadInts()
+        private static int[] ReadInts(TextReader reader)
         {
-            return Console.ReadLine().Split( ' ' ).Select( int.Parse ).ToArray();
+            return reader.ReadLine().Split(' ').Select(int.Parse).ToArray();
         }
     }
 }
