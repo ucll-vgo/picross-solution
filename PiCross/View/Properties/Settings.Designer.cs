5e251eb045b111d8f81c9e510982a1a9c7fe9f37
Frederic Vogels <frederic.vogels@ucll.be>
2019-03-02 14:59:51 +0100

    Reorganized projects

--- PiCross/View/Properties/Settings.Designer.cs
+++ PiCross/View/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Controls.Properties {
+namespace View.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
